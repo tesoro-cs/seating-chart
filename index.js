5ec42565fcb544eb048a3ff291925c6c274f56ec
Anthony <55033931+anthonyj33@users.noreply.github.com>
2022-10-17 11:01:01 -0700

    Work on fitText function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Todo
 - add undo for moving/etc -- var lastAction => move, create, assign, etc
+- fit text to input by changing font size -- figure out font size based on character width
 */
 
 var desks = 0;
@@ -9,12 +10,13 @@ function createDesk() {
     var newDesk = document.createElement("div");
     newDesk.id = desks;
     newDesk.classList = ["desk"];
-    var newLabel = document.createElement("p");
+    var newForm = document.createElement("form");
+    var newLabel = document.createElement("input");
     newLabel.id = "label" + desks;
     newLabel.classList = ["label"];
-    // newLabel.innerHTML = "-";
-    newLabel.contentEditable = "true";
-    newDesk.appendChild(newLabel);
+    newLabel.oninput = fitText;
+    newForm.appendChild(newLabel);
+    newDesk.appendChild(newForm);
     document.body.appendChild(newDesk);
     makeDraggable(document.getElementById(desks));
 }
@@ -58,3 +60,43 @@ function undo() {
         desks--;
     }
 }
+
+function fitText(i) {
+    i = i || window.event;
+    i = i.target;
+    var chars = i.value.length
+    var width = parseInt(window.getComputedStyle(i).getPropertyValue("width"));
+    var size = width/chars;
+    if(size <= 20) {
+        console.log(size);
+        i.style.fontSize = (width/chars) + "px";
+    } else {
+        i.style.fontSize = "20px";
+    }
+}
+
+/*
+Maybe this will work?
+
+const isOverflown = ({ clientHeight, scrollHeight }) => scrollHeight > clientHeight
+
+const resizeText = ({ element, elements, minSize = 10, maxSize = 512, step = 1, unit = 'px' }) => {
+  (elements || [element]).forEach(el => {
+    let i = minSize
+    let overflow = false
+
+        const parent = el.parentNode
+
+    while (!overflow && i < maxSize) {
+        el.style.fontSize = `${i}${unit}`
+        overflow = isOverflown(parent)
+
+      if (!overflow) i += step
+    }
+
+    // revert to last state where no overflow happened
+    el.style.fontSize = `${i - step}${unit}`
+  })
+}
+
+*/
